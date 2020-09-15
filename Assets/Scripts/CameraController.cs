234ea7fc622ddbdbb037377ca5d14ea2c02fb658
S4NDIX <sandix390@gmail.com>
2020-09-15 14:44:01 -0500

    Added rotation q&e
    Added rotation to rig but need to fix forward direction when rotate

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -8,13 +8,17 @@ public class CameraController : MonoBehaviour
     [SerializeField] private float fastSpeed;
     private float movementSpeed;
     [SerializeField] private float movementTime;
+    [SerializeField] private float rotationAmount;
 
     [SerializeField] Vector3 newPosition;
+
+    [SerializeField] Quaternion newRotation;
 
     // Start is called before the first frame update
     void Start()
     {
         newPosition = transform.position;
+        newRotation = transform.rotation;
     }
 
     // Update is called once per frame
@@ -27,7 +31,18 @@ public class CameraController : MonoBehaviour
     {
         //Detect if is pressing shift to move faster or not
         movementSpeed = Input.GetKey(KeyCode.LeftShift) ? fastSpeed : normalSpeed;
-        newPosition += new Vector3( Input.GetAxis("Vertical"),0,Input.GetAxis("Horizontal")*-1) * movementSpeed;
+        newPosition += new Vector3(Input.GetAxis("Horizontal"), 0,Input.GetAxis("Vertical")) * movementSpeed;
+
+        if (Input.GetKey(KeyCode.Q))
+        {
+            newRotation *= Quaternion.Euler(Vector3.up * rotationAmount);
+        } else if (Input.GetKey(KeyCode.E))
+        {
+            newRotation *= Quaternion.Euler(Vector3.up * -rotationAmount);
+        }
+
         transform.localPosition = Vector3.Lerp(transform.localPosition, newPosition, Time.deltaTime * movementTime);
+        transform.rotation = Quaternion.Lerp(transform.rotation, newRotation, Time.deltaTime * movementTime);
+
     }
 }
