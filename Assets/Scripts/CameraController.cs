c999ff22f6b4b910d5de1d9c78659858f4bef05d
S4NDIX <sandix390@gmail.com>
2020-09-15 14:28:45 -0500

    Added CamerController script
    Coded only axes movements TODO: Rotation with mouse or Q and E

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField] private float normalSpeed;
+    [SerializeField] private float fastSpeed;
+    private float movementSpeed;
+    [SerializeField] private float movementTime;
+
+    [SerializeField] Vector3 newPosition;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        newPosition = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        HandleMovementInput();
+    }
+
+    private void HandleMovementInput()
+    {
+        //Detect if is pressing shift to move faster or not
+        movementSpeed = Input.GetKey(KeyCode.LeftShift) ? fastSpeed : normalSpeed;
+        newPosition += new Vector3( Input.GetAxis("Vertical"),0,Input.GetAxis("Horizontal")*-1) * movementSpeed;
+        transform.localPosition = Vector3.Lerp(transform.localPosition, newPosition, Time.deltaTime * movementTime);
+    }
+}
